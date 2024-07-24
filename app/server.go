07e4ac6d983f3cbf8dabda881b4c588e2fd7753b
Neosh <neosh11@gmail.com>
2024-07-24 22:44:20 +1000

    update

--- app/server.go
+++ app/server.go
@@ -4,7 +4,93 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
+
+func getEndOfLine(startingIndex int, data []byte) (int, error) {
+	index := startingIndex
+	for index < len(data) && data[index-1] != '\r' && data[index] != '\n' {
+		index++
+	}
+	if index >= len(data) {
+		return -1, fmt.Errorf("Invalid data")
+	}
+	return index, nil
+}
+
+func processParameter(startIndex int, data []byte) (value string, EOL int, err error) {
+	// check if the first character is $
+	if data[startIndex] != '$' {
+		return "", -1, fmt.Errorf("Invalid data $")
+	}
+	// read the first line to get the length of the first argument
+	sizeEOL, err := getEndOfLine(startIndex+1, data)
+	if err != nil {
+		return "", -1, fmt.Errorf("Invalid data EOL - param")
+	}
+	length := string(data[startIndex+1 : sizeEOL-1])
+	lengthInt, err := strconv.Atoi(length)
+
+	if err != nil {
+		return "", -1, fmt.Errorf("Invalid data", length)
+	}
+	// ensure that the length of the first argument is correct
+	EOL = sizeEOL + 2 + lengthInt
+
+	if EOL > len(data) || data[EOL-1] != '\r' || data[EOL] != '\n' {
+		return "", -1, fmt.Errorf("Invalid data", string(data[sizeEOL+1:EOL-1]))
+	}
+	// read the first argument
+	return string(data[sizeEOL+1 : EOL-1]), EOL, nil
+}
+
+func redisProtocolParser(data []byte) (command string, arg1 string, err error) {
+	//*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
+	//	Read the first line to get the number of arguments
+	// check if the first character is *
+	if data[0] != '*' {
+		err = fmt.Errorf("Invalid data - *")
+		return
+	}
+
+	EOL, err := getEndOfLine(1, data)
+	if err != nil {
+		err = fmt.Errorf("Invalid data - EOL1")
+		return
+	}
+
+	numbParams := string(data[1 : EOL-1])
+	numbParamsInt, err := strconv.Atoi(numbParams)
+	if err != nil {
+		err = fmt.Errorf("Invalid data", numbParams)
+		return
+	}
+
+	if numbParamsInt > 2 {
+		err = fmt.Errorf("Only 1-2 parameters allowed at the moment")
+	}
+
+	// Read the first parameter which is the command
+	command, EOL, err = processParameter(EOL+1, data)
+	if err != nil {
+		err = fmt.Errorf("failed to process parameter", err)
+		return
+	}
+
+	if numbParamsInt == 1 {
+		return
+	}
+	if numbParamsInt == 2 {
+		// Read the second parameter
+		arg1, EOL, err = processParameter(EOL+1, data)
+		if err != nil {
+			err = fmt.Errorf("failed to process parameter", err)
+			return
+		}
+	}
+	fmt.Println("Command: ", command)
+	return
+}
 
 func processResponse(req net.Conn) error {
 	// read the data from the connection
@@ -14,10 +100,22 @@ func processResponse(req net.Conn) error {
 		return err
 	}
 
-	fmt.Println("Received: ", string(data[:read]))
+	command, arg1, err := redisProtocolParser(data[:read])
+	if err != nil {
+		return err
+	}
 
-	// send back the response: +PONG\r\n
-	_, err = req.Write([]byte("+PONG\r\n"))
+	if command == "PING" {
+		_, err = req.Write([]byte("+PONG\r\n"))
+	} else if command == "ECHO" {
+		if arg1 == "" {
+			_, err = req.Write([]byte("$0\r\n\r\n"))
+		} else {
+			_, err = req.Write([]byte("$" + strconv.Itoa(len(arg1)) + "\r\n" + arg1 + "\r\n"))
+		}
+	} else {
+		_, err = req.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
+	}
 	return err
 }
 
@@ -28,7 +126,6 @@ func handleConnection(req net.Conn) {
 			fmt.Println("Error closing connection: ", err.Error())
 		}
 	}()
-
 	for {
 		err := processResponse(req)
 		if err != nil {
