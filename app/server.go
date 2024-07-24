bbec31400935d19d7db32b24f19b9737ff0bb8df
Neosh <neosh11@gmail.com>
2024-07-24 21:20:48 +1000

    Add a PONG for requests

--- app/server.go
+++ app/server.go
@@ -9,13 +9,17 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	req, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	
+	req.Write([]byte("+PONG\r\n"))
 }
